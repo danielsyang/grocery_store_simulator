e93a995b05edcf7966d0445c56f0489c3f987470
Daniel Yang <shu.minyang@hotmail.com>
2025-05-13 21:56:59 -0400

    add items to inventory

--- scenes/inventory_gui/inventory_slot.gd
+++ scenes/inventory_gui/inventory_slot.gd
@@ -2,12 +2,12 @@ extends Panel
 
 @onready var background_sprite: Sprite2D = $Background
 @onready var item_sprite: Sprite2D = $Item
+@onready var item_quantity: Label = $Item/Quantity
 
 func update_item(item: InventoryItem):
-	if item == null:
-		background_sprite.frame = 0
-		item_sprite.visible = false
-	else:
-		background_sprite.frame = 1
-		item_sprite.visible = true
-		item_sprite.texture = item.texture
+	background_sprite.frame = 1
+	item_sprite.visible = true
+	item_sprite.texture = item.texture
+	item_quantity.visible = true
+	item_quantity.text = str(1)
+		
